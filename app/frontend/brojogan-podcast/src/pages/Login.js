b38cf6085421069d85f2a94d3a685e73b2a055fa
jmk11 <z5160822@student.unsw.edu.au>
2020-10-11 20:40:12 +1100

    start login, signup event handlers

--- app/frontend/brojogan-podcast/src/pages/Login.js
+++ app/frontend/brojogan-podcast/src/pages/Login.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './../css/Login.css';
 import logo from './../images/logo.png';
+import {API_URL} from './../constants';
+
+function loginHandler() {
+  const form = document.forms['login-form'];
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+  // check for maximum length? check that they don't violate some constraints?
+  if (username && password) {
+    let formData = new FormData(form);
+    fetch(`${API_URL}/login`, {method: 'post', body: formData})
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error('Login failed');
+        }
+      })
+      .then(json => {
+        document.cookie = `token={json.token}`;
+        // redirect to homepage
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+}
 
 function Login() {
-  function handler() {
-    console.log('!yo');
-  }
   return (
     <div id="wrapper">
       <div id='login-div'>
@@ -25,11 +48,11 @@ function Login() {
           <h1>Log In</h1>
           <form id="login-form">
             <p id="username-text">Username</p>
-            <input type="text" id="username-input"/>
+            <input type="text" id="username-input" name="username"/>
             <p id="password-text">Password</p>
-            <input type="text" id="password-input"/>
+            <input type="password" id="password-input" name="password"/>
             <div id="form-btns">
-              <button id="logIn-btn" type="button" onClick={handler}>Log In</button>
+              <button id="logIn-btn" type="button" onClick={loginHandler}>Log In</button>
               <button id="signUp-btn" type="button">Sign Up</button> 
             </div>
           </form>
--- app/frontend/brojogan-podcast/src/pages/SignUp.js
+++ app/frontend/brojogan-podcast/src/pages/SignUp.js
@@ -1,6 +1,67 @@
 import React from 'react';
 import './../css/SignUp.css';
 import logo from './../images/logo.png';
+import {API_URL} from './../constants';
+
+function signupHandler() {
+  const form = document.forms['signUp-form'];
+  const username = form.elements.username.value;
+  const email = form.elements.email.value;
+  const password1 = form.elements.password1.value;
+  const password2 = form.elements.password2.value;
+
+  function usernameValid(username) {
+    return true;
+  }
+  function passwordValid(password) {
+    return true;
+  }
+
+  // put username validity and password requirements in the html?
+  if (username && password1 && password2 && email) {
+    if (! usernameValid(username)) {
+      alert('Invalid username');
+    } else if (! passwordValid(password1)) {
+      alert('Invalid password');
+    } else if (password1 !== password2) {
+      alert('Passwords not matching')
+    } else {
+      let body = {"username": username, "email": email, "password": password1};
+      fetch(`${API_URL}/users`, {method: 'post', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(body)})
+        .then(resp => {
+          resp.json().then(data => {
+            if (resp.status === 201) {
+              document.cookie = data.token;
+              // redirect to homepage
+            } else {
+              alert(data.error);
+            }
+          })
+        })
+        .catch(error => { // will this catch error from resp.json()?
+          alert('Network error or something bad');
+        });
+    }
+
+    // let formData = new FormData(form);
+    // fetch(`{API_URL}/login`, {method: 'post', body: formData})
+    //   .then(resp => {
+    //     if (resp.ok) {
+    //       return resp.json();
+    //     } else {
+    //       throw new Error('Login failed');
+    //     }
+    //   })
+    //   .then(json => {
+    //     document.cookie = `token={json.token}`;
+    //     // redirect to homepage
+    //   })
+    //   .catch(error => {
+    //     alert(error);
+    //   });
+  }
+}
+
 
 function SignUp() {
   return (
@@ -23,17 +84,21 @@ function SignUp() {
           <form id="signUp-form">
             <div id="username-div">
               <p id="username-text">Username</p>
-              <input type="text" id="username-input"/>
+              <input type="text" id="username-input" name="username"/>
+            </div>
+            <div>
+              <p id="email-text">Email</p>
+              <input type="text" id="email-input" name="email"/>
             </div>
             <div>
               <p id="password-text">Password</p>
-              <input type="text" id="password-input"/>
+              <input type="password" id="password-input" name="password1"/>
             </div>
             <div>
               <p id="password-text">Confirm Password</p>
-              <input type="text" id="password-input"/>
+              <input type="password" id="password-input" name="password2"/>
             </div>
-            <button id="signUp-btn-2" type="button">Sign Up</button>
+            <button id="signUp-btn-2" type="button" onClick={signupHandler}>Sign Up</button>
           </form>
         </div>
       </div>
