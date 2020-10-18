d5b9eb3e8de946db179d2c1b68ae508a3827a965
bowdens <tomfbowden12@gmail.com>
2020-10-18 19:29:03 +1100

    searching works

--- app/frontend/brojogan-podcast/src/pages/SearchPage.js
+++ app/frontend/brojogan-podcast/src/pages/SearchPage.js
@@ -5,11 +5,18 @@ function Search(podcasts) {
 	var query = window.location.search.substring(1);
 	console.log("starting query")
 	fetch(`${API_URL}/podcasts?search_query=`+query+'&offset=0&limit=50', {method: 'get'})
-	.then(resp => {
-          resp.json().then(podcasts => {})});
+	.then(resp => resp.json())
+    .then(podcasts => {
+        const podcastList = document.getElementById("podcast-list");
+        for (let podcast of podcasts) {
+            let newLi = document.createElement("li");
+            newLi.innerText = podcast.title + " | Subscribers: " + podcast.subscribers;
+            podcastList.appendChild(newLi);
+        }
+    });
     return (
         <div>
-            <ul>
+            <ul id="podcast-list">
             </ul>
         </div>
     )
