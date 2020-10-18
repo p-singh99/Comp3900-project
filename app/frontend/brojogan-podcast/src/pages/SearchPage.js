b4c883f58ce5522d15bdf796ce36aab5908a029e
NichBang <30569168+NichBang@users.noreply.github.com>
2020-10-18 20:20:48 +1100

    Update SearchPage.js

--- app/frontend/brojogan-podcast/src/pages/SearchPage.js
+++ app/frontend/brojogan-podcast/src/pages/SearchPage.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {API_URL} from './../constants';
 
 function Search(podcasts) {
-	var query = window.location.search.substring(1);
-	console.log("starting query")
-	fetch(`${API_URL}/podcasts?search_query=`+query+'&offset=0&limit=50', {method: 'get'})
-	.then(resp => resp.json())
+        var query = window.location.search.substring(1);
+        console.log("starting query")
+        fetch(`${API_URL}/podcasts?search_query=`+query+'&offset=0&limit=50', {method: 'get'})
+        .then(resp => resp.json())
     .then(podcasts => {
         const podcastList = document.getElementById("podcast-list");
         for (let podcast of podcasts) {
-            let newLi = document.createElement("li");
-            newLi.innerText = podcast.title + " | Subscribers: " + podcast.subscribers;
-            podcastList.appendChild(newLi);
+              let newLi = document.createElement("li");
+              var text = podcast.title + " | Subscribers: " + podcast.subscribers;
+              var a = document.createElement("a");
+              a.textContent = text;
+              a.href = "localhost:3000/podcast/"+podcast.pid; 
+              newLi.appendChild(a);
+              podcastList.appendChild(newLi);
         }
     });
     return (
