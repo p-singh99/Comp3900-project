1018da56602bfe8935140808e045aae10573962a
bowdens <tomfbowden12@gmail.com>
2020-10-22 19:12:09 +1100

    working on playback

--- app/frontend/brojogan-podcast/src/components/Footer.js
+++ app/frontend/brojogan-podcast/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { HowlWrapper, currentlyPlaying } from './../playing.js';
 import './../css/Footer.css';
 
 // function storeTime() {
@@ -9,30 +10,18 @@ function sendTime() {
   // get playtime, send to server
 }
 
-// function Footer({ playing, setPlaying }) {
-  function Footer() {
-  // const [episode, setEpisode] = useState({src: "https://dts.podtrac.com/redirect.mp3/feeds.soundcloud.com/stream/911699122-chapo-trap-house-463-teaser-mods-will-not-save-us.mp3", podcastId: null, episodeGuid: null});
-  // window.setTimeout(storeTime, 10*1000);
-  // window.setTimeout(sendTime, 30*1000);
-
-  // edit src from other components somehow
-  // and only display if set
-
-  // if (!playing) {
-  //   return null;
-  // }
-  
+function Footer() {
   return (
-    // <div id='footer-div' style={{display: playing ? 'block' : 'none'}}>
     <div id='footer-div'>
-      {/* <h1>Footer</h1> */}
-      <div><audio src='' controls autoPlay></audio>
-      <button>Stop</button></div>
-      {/* {playing
-        ? <div><audio src={playing.src} controls autoPlay></audio>
-          <button onClick={() => setPlaying()}>Stop</button></div>
-        : null
-      } */}
+      <div id='player'>
+        <img src="" class="thumbnail"></img>
+        <span class="material-icons" id='playpause'>play_arrow</span>
+        <div class="seekbar">
+          seek bar
+        </div>
+        <span class="material-icons" id="volume">volume_up</span>
+        <span class="material-icons" id="rate">speed</span>
+      </div>
     </div>
   )
 }
