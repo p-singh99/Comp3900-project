1b5335c086480221c931054f1f5750905f68b89b
Michael <z5206453@unsw.edu.au>
2020-11-12 14:17:22 +1100

    change history fetch

--- app/frontend/brojogan-podcast/src/pages/History.js
+++ app/frontend/brojogan-podcast/src/pages/History.js
@@ -29,10 +29,10 @@ import './../css/History.css';
 
 function History() {
   async function fetchItems(pgNum, signal) {
-    // const data = await fetchAPI(`/self/history/${pgNum}`, 'get', null, signal);
+    const data = await fetchAPI(`/self/history/${pgNum}`, 'get', null, signal);
     const pageSize = 12;
     const offset = (pgNum-1)*pageSize;
-    const data = await fetchAPI(`/self/history?offset=${offset}&limit=${pageSize}`, 'get', null, signal);
+    //const data = await fetchAPI(`/self/history?offset=${offset}&limit=${pageSize}`, 'get', null, signal);
     console.log("History data:", data);
     console.log(data.numPages);
     if (pgNum === 1) {
