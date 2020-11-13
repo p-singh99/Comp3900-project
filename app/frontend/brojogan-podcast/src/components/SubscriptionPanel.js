78c071d8f1a56e20d8b6836dc876930300ce3006
Michael <z5206453@unsw.edu.au>
2020-11-13 22:05:54 +1100

    change route

--- app/frontend/brojogan-podcast/src/components/SubscriptionPanel.js
+++ app/frontend/brojogan-podcast/src/components/SubscriptionPanel.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {isLoggedIn, fetchAPI} from './../auth-functions';
+import {isLoggedIn, fetchAPI} from './../authFunctions';
 import {getPodcastFromXML} from './../rss';
 import EpisodeComponent from './EpisodeComponent';
 
