1b2e643f3112bbaa818bcafbe48153ab7c51f546
Michael <z5206453@unsw.edu.au>
2020-11-15 22:13:22 +1100

    just one line

--- app/backend/main.py
+++ app/backend/main.py
@@ -484,11 +484,7 @@ class Recommendations(Resource):
 		recs = []
 		cur.execute("select distinct * from recommendations(%s)", (user_id,))
 		results = cur.fetchall()
-<<<<<<< HEAD
 		recs = [{"title": i[0], "thumbnail": i[1], "id": i[2], "subs": i[3], "eps": i[4], "rating": f"{i[5]:.1f}"} for i in results]
-=======
-		[recs.append({"title": i[0], "thumbnail": i[1], "id": i[2], "subs": i[3], "eps": i[4], "rating":  f"{i[5]:.1f}"}) for i in results]
->>>>>>> 2416e318246d7535ab2ee198eb941115256a22b2
 		df.close_conn(conn,cur)
 		return {"recommendations" : recs}
 
@@ -630,8 +626,8 @@ class BestPodcasts(Resource):
 		conn, cur = df.get_conn()
 		cur.execute("SELECT p.id, p.xml, p.count, t.thumbnail, r.coalesce FROM podcastsubscribers p, podcasts t, ratingsview r ORDER BY p.count DESC Limit 10")
 		top_subbed = [{"id": i[0], "xml": i[1], "subs": i[2], "thumbnail": i[3], "rating": i[4]} for i in cur.fetchall()]
-		cur.execute("SELECT * FROM ratingsview ORDER BY rating DESC Limit 10")
-		top_rated = cur.fetchall()
+		cur.execute("SELECT p.id, p.xml, p.count, t.thumbnail, r.coalesce FROM podcastsubscribers p, podcasts t, ratingsview r ORDER BY p.count DESC Limit 10")
+		top_rated = [{"id": i[0], "xml": i[1], "subs": i[2], "thumbnail": i[3], "rating": i[4]} for i in cur.fetchall()]
 		df.close_conn(conn,cur)
 		return {"topSubbed": top_subbed, "topRated": top_rated}, 200
 
