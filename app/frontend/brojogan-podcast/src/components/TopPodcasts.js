89f3f36d89b1fd30b27f9f6f4269ab2457389f73
Pawanjot Singh <z5207377@unsw.edu.au>
2020-11-16 15:08:41 +1100

    Changes

--- app/frontend/brojogan-podcast/src/components/TopPodcasts.js
+++ app/frontend/brojogan-podcast/src/components/TopPodcasts.js
@@ -3,22 +3,22 @@ import { fetchAPI } from '../authFunctions';
 import PodcastCards from './PodcastCards';
 
 function TopPodcasts() {
-  const [topSubbed, setTopSubbed] = useState([]);
-  const [topRated, setTopRated] = useState([]);
+  const [topSubbed, setTopSubbed] = useState({});
+  const [topRated, setTopRated] = useState({});
 
   const loadTopPodcasts = () => {
-    // setTopSubbed('Loading Top Subbed');
-    // setTopRated('Loading Top Rated');
+    setTopSubbed('Loading Top Subbed');
+    setTopRated('Loading Top Rated');
 
     let results = fetchAPI('/top-podcasts');
     results.then(items => {
       console.log(`Top Result is: ${JSON.stringify(items.topSubbed)}`);
       for (let item of items.topSubbed ) {
         console.log(JSON.stringify(item));
-      }
+      } 
       setTopSubbed(items.topSubbed);
     });
-  }
+  }  
 
   useEffect(() => {
     loadTopPodcasts();
--- app/frontend/brojogan-podcast/src/pages/Home.js
+++ app/frontend/brojogan-podcast/src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import { isLoggedIn, getUsername } from './../authFunctions'
 import { Helmet } from 'react-helmet';
 import SubscriptionPanel from './../components/SubscriptionPanel';
@@ -17,22 +18,54 @@ function welcome(newUser) {
 }
 
 function Home(props) {
+  const [windowDimensions, setWindowDimensions] = useState({});
   const newUser = props.location.state ? props.location.state.newUser : false;
+  
+  const handleResize = () => {
+    console.log(`Resized to: ${window.innerWidth} X ${window.innerHeight}`);
+  }
+  
+  window.addEventListener('resize', handleResize);
   return (
     <div>
       <Helmet>
         <title>Brojogan Podcasts</title>
       </Helmet>
 
-      {/* <div id="new-user-login">
-
-      </div> */}
+      <div id="new-user-login">
+        <div id="message">
+          <p id="message-1">
+            {/* <h4> */}
+              Subscribe And Get Notified
+              {/* Create an account with us */}
+            {/* </h4> */}
+          </p>
+          <p id="message-2">
+              Create an account with us <br/>
+          </p>
+          <p id="message-3">
+              Its Free!
+          </p>
+          <div id="form-homePage">
+            <Link to={{ pathname: `/login`}}>
+              <button type="button" id="login-homePage" className="homePage-btn">
+                Login
+              </button>
+            </Link>
+            <Link to={{ pathname: `/signup`}}>
+              <button type="button" id="signup-homePage" className="homePage-btn">
+                Sign Up
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
 
       <h1>
         {/* {welcome(newUser)} */}
       </h1>
       {/* < SubscriptionPanel/> */}
-      <TopPodcasts />
+      {/* <TopPodcasts /> */}
     </div>
   );
 }
