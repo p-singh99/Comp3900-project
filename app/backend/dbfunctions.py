a0b66bf3def77882cb529021e81042a7ddc3f803
Tom Bowden <tomfbowden12@gmail.com>
2020-11-16 21:47:37 +1100

    properly added environ variables

--- app/backend/dbfunctions.py
+++ app/backend/dbfunctions.py
@@ -2,7 +2,7 @@ from SemaThreadPool import SemaThreadPool
 import os
 
 conn_pool = None
-if os.environ.get("BROJOGAN_USE_LOCAL") == 1:
+if os.environ.get("BROJOGAN_USE_LOCAL") == "1":
     conn_pool = SemaThreadPool(1,50,dbname="ultracast")
 else:
     conn_pool = SemaThreadPool(1, 50,\
