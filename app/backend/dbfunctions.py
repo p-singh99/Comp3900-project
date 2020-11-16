7a28973bbb0974c67d593803a145dec79ac22c2c
Tom Bowden <tomfbowden12@gmail.com>
2020-11-16 21:42:35 +1100

    added environment variable for local database

--- app/backend/dbfunctions.py
+++ app/backend/dbfunctions.py
@@ -1,7 +1,12 @@
 from SemaThreadPool import SemaThreadPool
+import os
 
-conn_pool = SemaThreadPool(1, 50,\
-	 dbname="ultracast", user="brojogan", password="GbB8j6Op", host="polybius.bowdens.me", port=5432)
+conn_pool = None
+if os.environ.get("BROJOGAN_USE_LOCAL") == 1:
+    conn_pool = SemaThreadPool(1,50,dbname="ultracast")
+else:
+    conn_pool = SemaThreadPool(1, 50,\
+             dbname="ultracast", user="brojogan", password="GbB8j6Op", host="polybius.bowdens.me", port=5432)
 
 def get_conn():
 	conn = conn_pool.getconn()
